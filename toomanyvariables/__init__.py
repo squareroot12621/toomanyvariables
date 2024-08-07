5383e93450054bcac71b38f8afe3c1aa8d756a68
squareroot12621 <mathandcoding2718@gmail.com>
2024-08-07 17:55:38 -0500

    Add the MIT license and the README.md link.

--- toomanyvariables/__init__.py
+++ toomanyvariables/__init__.py
@@ -1,6 +1,34 @@
+# MIT License
+#
+# Copyright (c) 2024 squareroot12621
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# This code has been modified from code, made by squareroot12621, under
+# the Creative Commons Zero v1.0 Universal license. The original code
+# can be located at
+# https://github.com/squareroot12621/messenger-language/blob/main/messenger/__init__.py .
+
 """Too Many Variables! is an esoteric programming language.
 For more information, see the README.md at
-<link>.
+https://github.com/squareroot12621/toomanyvariables/blob/main/README.md .
 """
 
 import argparse
